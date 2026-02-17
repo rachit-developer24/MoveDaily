083bab7b98a3551bd5036d2fbc4337a17eceb492
rachit-developer24 <srachit841@gmail.com>
2026-02-17 01:27:03 +0000

    Initial Commit

---
+++ MoveDaily/MoveDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveDailyApp.swift
+//  MoveDaily
+//
+//  Created by Rachit Sharma on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoveDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
